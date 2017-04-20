92ce9ba1bcf540f8695e3f56db5c995226d342da
Jack Blankenship <jack.blankenship@gmail.com>
2017-04-19 18:43:08 -0700

    full error route enabled
    code changed that should handle all errors and render the error.pug
    page.

--- src/app.js
+++ src/app.js
@@ -34,53 +34,42 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res, next){
 
-	//	set defaults because not everyone is a twitter master. Also, I can soft fail network failures.
-	let timeline = [{ "text": "Massive amount of information overload trying Express/NodeJS",
-		 "user": {"profile_image_url": "/static/images/m-spore.png", "screen_name": "jackblankensh13", "name": "Jack Blankenship"}, 
-		 "created_at": "2017-04-15", "favorite_count": 260, "retweet_count": 42 },
-		  { "text": "But enough about me, how about those @Treehouse teachers?",
-		 "user": {"profile_image_url": "/static/images/m-spore.png", "screen_name": "jackblankensh13", "name": "Jack Blankenship"}, 
-		 "created_at": "2017-04-15", "favorite_count": 789, "retweet_count": 33 }
-		 ];
-	let following = '';
+	//	set message defaults because that interface is not responding for some reason. 
+	let timeline = [];
+	let following = [];
 	let messages = [{ "sender": {"profile_image_url": "/static/images/m-spore.png"}, "text": " Our appologies. We failed to get your direct messages.", "created_at": "2017-04-15"}];
-	/*
-	// we dont talk about promises for 3 more units. /sadface
-	Promise.all([ 
-		req.timelineTweets.then(function(result) {  timeline = result.data }),	 
-		req.followingUsers.then(function(result) { following = result.data }),
-		req.directMessages.then(function(result) {  messages = result.data })
-		])
-	.then (([timeline , following , messages]) => {
-		res.render(__dirname + '/templates/index.pug', {timeline: timeline, following: following, messages: messages});
-	})
-	.catch (err => {
-		// something went wrong
-	});
-	*/
-	req.timelineTweets.then(function (result) {
-        timeline = result.data;
-        var myScreenName = timeline[0].user.screen_name;
-    	
-        req.directMessages.then(function (result) {
-        	// not sure if its because my app settings are still wrong in regards to direct messages or what.
-        	if ((Object.getOwnPropertyNames(result.data) === 'errors') || (result.errors != "")) {	        			// because you may be totally new to twitter.
-        	} else {
-	            messages = result.data;
-	        }
 
-            req.followingUsers.then(function (result) {
-            	following = result.data.users;
-	            // rendering page and passing twit objects with the response
-                res.render(__dirname + '/templates/index.pug',{timeline: timeline,
-                                                                following: following,
-                                                                messages: messages,
-                                                                myScreenName: myScreenName});
-            })
-        })
-    })
+	req.timelineTweets.then(function (result) {
+		console.log(Object.getOwnPropertyNames(result.data));
+    	if ( (Object.getOwnPropertyNames(result.data)[0] === 'length' ) || (Object.getOwnPropertyNames(result.data.length) == [] ) ) {
+    		console.log('trying to throw error');
+    		var err = new Error;
+    		err.status = 503;
+    		err.message = 'Our network is busy, try again in a few minutes';
+    		return next(err);
+    	} else {
+	        timeline = result.data;
+	        var myScreenName = timeline[0].user.screen_name;
+	        req.directMessages.then(function (result) {
+	        	// not sure if its because my app settings are still wrong in regards to direct messages or what.
+	        	if ((Object.getOwnPropertyNames(result.data) === 'errors') || (result.errors != "")) {	        			// because you may be totally new to twitter.
+	        	} else {
+		            messages = result.data;
+		        }
+
+	            req.followingUsers.then(function (result) {
+	            	following = result.data.users;
+		            // rendering page and passing twit objects with the response
+	                res.render(__dirname + '/templates/index.pug',{timeline: timeline,
+	                                                                following: following,
+	                                                                messages: messages,
+	                                                                myScreenName: myScreenName});
+	            })		// end followingUsers
+	        });			// end directmessages
+	    }
+    })					// end timeline
 });
 
 app.get('/tweet/:data', function (req, res ) {
@@ -92,23 +81,27 @@ app.get('/tweet/:data', function (req, res ) {
 });
 //*************************************
 // This next app.get must come last
-// DO NOT have a next() in the above app.get
+// DO NOT have a next() in the above app.get unless you have tossed an ERROR
 //*************************************
 app.get('/*', function (req, res, next ) {
 	var err = new Error();
-	err.status = 404;
-	next(err);
-	//res.redirect('/'); // this just redirects to main route.
+	err.status = 404;				// set status to 404 which is page not found. 
+	next(err);						// drop into the error routine below.
 });
 
 app.use(function(err, req, res, next) {
+	let thisImage = '/static/images/starwars404.jpg';
+	let thisMessage;
 	if(err.status !== 404) {
-		return next();
+		thisImage = '/static/images/technicaldifficulties.jpeg'
+		thisMessage = err.status +': ' + err.message || 'we are experiencing technical difficulties';
+		res.status = err.status;
+	} else {
+		thisMessage = err.message || '** page not found **';
+		res.status(404);
 	}
- 	let thisMessage = err.message || '** page not found **';
 
-	res.status(404);
-	res.render(__dirname + '/templates/error.pug', {thisMessage: thisMessage});
+	res.render(__dirname + '/templates/error.pug', {thisMessage: thisMessage, thisImage: thisImage});
 });
 
 app.listen(3000, function(){
